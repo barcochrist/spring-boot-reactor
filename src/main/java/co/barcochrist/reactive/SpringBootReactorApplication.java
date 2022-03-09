f622d6f142168dc7e6f8c286dfcaa9ed0476977e
Christian Barco <barcochrist@gmail.com>
2022-03-09 12:25:46 -0500

    (basic) add our first reactive stream and print values in console

--- src/main/java/co/barcochrist/reactive/SpringBootReactorApplication.java
+++ src/main/java/co/barcochrist/reactive/SpringBootReactorApplication.java
@@ -1,13 +1,27 @@
 package co.barcochrist.reactive;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import reactor.core.publisher.Flux;
 
 @SpringBootApplication
-public class SpringBootReactorApplication {
+public class SpringBootReactorApplication implements CommandLineRunner {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringBootReactorApplication.class, args);
-	}
+  private static final Logger log = LoggerFactory.getLogger(SpringBootReactorApplication.class);
 
+  public static void main(String[] args) {
+    SpringApplication.run(SpringBootReactorApplication.class, args);
+  }
+
+  @Override
+  public void run(String... args) throws Exception {
+    Flux<String> names = Flux
+        .just("Christian", "Andres", "Camila", "Sara")
+        .doOnNext(System.out::println);
+
+    names.subscribe(log::info);
+  }
 }
