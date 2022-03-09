eb7977e0c48f09131aa3fc3d7bd5eb0454dc2307
Christian Barco <barcochrist@gmail.com>
2022-03-09 11:49:02 -0500

    (init) create basic project structure

---
+++ src/main/java/co/barcochrist/reactive/SpringBootReactorApplication.java
@@ -0,0 +1,13 @@
+package co.barcochrist.reactive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootReactorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootReactorApplication.class, args);
+	}
+
+}
